4178a835596ea81dbe19c91a7b72ea403d408698
Ajay Kiran <ajaykc@microland.com>
2019-08-01 09:46:19 +0000

    Initial Commit

---
+++ src/webparts/one/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IOneWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'OneWebPartStrings' {
+  const strings: IOneWebPartStrings;
+  export = strings;
+}
